b48e48f20588d5cfa3b2d15fee8a6db9304719d4
jsged <jsgedwards@outlook.com>
2025-02-28 18:17:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import itertools
 def get_combinations(format_string):
     # Define possible characters
     digit_chars = "0123456789"
-    letter_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letter_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     # Create a list of possible character sets for each position
     char_sets = [digit_chars if char == 'X' else letter_chars if char == '-' else [char] for char in format_string]
@@ -13,19 +13,29 @@ def get_combinations(format_string):
     # Generate all combinations
     return (''.join(combo) for combo in itertools.product(*char_sets))
 
-def main():
+def generator():
     # Get format from user
     format_string = input("Enter password format (use 'X' for digits and '-' for letters): ")
+    printed = int(input("Would you like to print the combinations as theyre generated? (1 for yes, 0 for no): "))
 
-    # Generate all possible passwords
-    combinations = get_combinations(format_string)
+    if printed == 1:
+        # Generate all possible passwords
+        combinations = get_combinations(format_string)
 
-    # Write to a file
-    with open("password_combinations.txt", "w") as file:
-        for password in combinations:
-            file.write(password + "\n")
+        # Write to a file
+        with open("password_combinations.txt", "w") as file:
+            for password in combinations:
+                file.write(password + "\n")
+                print(password)
+    elif printed == 0:
+        # Generate all possible passwords
+        combinations = get_combinations(format_string)
+
+        # Write to a file
+        with open("password_combinations.txt", "w") as file:
+            for password in combinations:
+                file.write(password + "\n")
 
     print("All possible combinations have been written to password_combinations.txt")
-
-if __name__ == "__main__":
-    main()
+    
+generator()
